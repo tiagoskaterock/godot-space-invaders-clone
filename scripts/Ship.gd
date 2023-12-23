ab8671f02ea743abe8fe79563e18e59270cb1b2f
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-23 08:23:27 -0300

    138. 09 - Criando a nave parte 3 - Limitação nas bordas do jogo

--- scripts/Ship.gd
+++ scripts/Ship.gd
@@ -16,8 +16,10 @@ func check_shoot():
 	
 func check_movement(delta):
 	var dir = 0
-	if Input.is_action_pressed("go_right"): dir += 1
-	if Input.is_action_pressed("go_left"): dir -= 1 
+	if Input.is_action_pressed("go_right") and get_global_pos().x < 173: 
+		dir += 1
+	if Input.is_action_pressed("go_left") and get_global_pos().x > 7: 
+		dir -= 1 
 	translate(Vector2(1, 0) * SPEED * dir * delta)
 	
 func shoot():
