e33b4f56c3a5df199ac7f67f02e04b843cad0fff
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-23 08:13:25 -0300

    137. 08 - Criando a nave parte 2 - Movimentação

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# godot-space-invaders-clone
-A game made in Godot Engine inspired by the classic arcade Space Invaders
+<h1 align="center">Space Invaders Clone</h1>
+
+<p align="center">
+  <a href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=godot" />
+  </a>
+</p>
+
+<p align="center">
+    Download Godot 2, import it in your projects and run.
+</p>
+
+<p align="center">
+    <img src="1.png" alt="Star Shooter" />
+</p>
---
+++ scripts/Ship.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+const SPEED = 100
+var left
+var right
+
+func _ready():
+	set_process(true)
+	
+func _process(delta):	
+	check_shoot()
+	check_movement(delta)
+	
+func check_shoot():
+	if Input.is_action_pressed("shoot"): shoot()
+	
+func check_movement(delta):
+	var dir = 0
+	if Input.is_action_pressed("go_right"): dir += 1
+	if Input.is_action_pressed("go_left"): dir -= 1 
+	translate(Vector2(1, 0) * SPEED * dir * delta)
+	
+func shoot():
+	print('shoot')
