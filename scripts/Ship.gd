d611d647d0f5052e22435fe4a3f9990eabe7e604
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-24 06:37:23 -0300

    141. 12 - Efetuando disparos lazer

--- scripts/Ship.gd
+++ scripts/Ship.gd
@@ -1,19 +1,24 @@
 extends Area2D
 
 const SPEED = 100
-var left
-var right
+var prev_shoot = preload("res://scenes/Shoot.tscn")
+var prev_laser = false
 
 func _ready():
 	set_process(true)
 	
-func _process(delta):	
-	check_shoot()
+func _process(delta):
 	check_movement(delta)
 	
-func check_shoot():
-	if Input.is_action_pressed("shoot"): shoot()
+	if Input.is_action_pressed("shoot") and ! prev_laser:		
+		var shoot = prev_shoot.instance()
+		get_parent().add_child(shoot)
+		shoot.set_global_pos(get_global_pos())
+		print(shoot.get_global_pos().y)
 	
+	prev_laser = Input.is_action_pressed("shoot")
+		
+
 func check_movement(delta):
 	var dir = 0
 	if Input.is_action_pressed("go_right") and get_global_pos().x < 173: 
@@ -22,5 +27,3 @@ func check_movement(delta):
 		dir -= 1 
 	translate(Vector2(1, 0) * SPEED * dir * delta)
 	
-func shoot():
-	print('shoot')
